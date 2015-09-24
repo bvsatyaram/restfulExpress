0d3ae022b63d8698da72841f940bfee970361209
BV Satyaram <bvsatyaram@gmail.com>
2015-09-25 04:50:34 +0530

    Create PUT and PATCH requests to handle updating a book

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -18,18 +18,50 @@ var routes = function(Book) {
           res.status(500).send(err);
         else
           res.json(books);
+      });
     });
-  });
 
-  bookRouter.route(' /:bookId')
+  bookRouter.route('/:bookId')
     .get(function(req, res) {
       Book.findById(req.params.bookId, function(err, book) {
         if(err)
           res.status(500).send(err);
         else
           res.json(book);
+      });
+    })
+    .put(function(req, res) {
+      Book.findById(req.params.bookId, function(err, book) {
+        if(err) {
+          res.status(500).send(err);
+        } else {
+          book.title = req.body.title;
+          book.author = req.body.author;
+          book.genre = req.body.genre;
+          book.read = req.body.read;
+          book.save();
+          res.json(book);
+        }
+      });
+    })
+    .patch(function(req, res) {
+      Book.findById(req.params.bookId, function(err, book) {
+        if(err) {
+          res.status(500).send(err);
+        } else {
+          if(req.body.title != undefined)
+            book.title = req.body.title;
+          if(req.body.author != undefined)
+            book.author = req.body.author;
+          if(req.body.genre != undefined)
+            book.genre = req.body.genre;
+          if(req.body.read != undefined)
+            book.read = req.body.read;
+          book.save();
+          res.json(book);
+        }
+      });
     });
-  });
 
   return bookRouter;
 };
