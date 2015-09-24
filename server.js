d637f1d752b9233c6ed423060a6107c165c5a6d6
BV Satyaram <bvsatyaram@gmail.com>
2015-09-24 05:33:30 +0530

    Removing accidentally added file

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-    app = express();
-
-app
-  .use(express.static('./public'))
-  .get('*', function(req, res) {
-    res.sendfile('public/main.html');
-  })
-  .listen(3000);
