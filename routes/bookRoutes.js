37bb6843a5a38e4bdca7334c0279e4cda55229dd
BV Satyaram <bvsatyaram@gmail.com>
2015-09-25 05:32:44 +0530

    Introducing booksController

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,25 +1,10 @@
 var express = require('express');
 var routes = function(Book) {
   var bookRouter = express.Router();
+  var booksController = require('../controllers/booksController')(Book);
   bookRouter.route('/')
-    .post(function(req, res) {
-      var book = new Book(req.body);
-
-      book.save();
-      res.status(201).send(book);
-    })
-    .get(function(req, res) {
-      var query = {};
-      if (req.query.genre) {
-        query.genre = req.query.genre;
-      }
-      Book.find(query, function(err, books) {
-        if(err)
-          res.status(500).send(err);
-        else
-          res.json(books);
-      });
-    });
+    .post(booksController.post)
+    .get(booksController.get);
 
 
   bookRouter.use('/:bookId', function(req, res, next) {
